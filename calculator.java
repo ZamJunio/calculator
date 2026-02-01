48d8d9de4eeeb4db17754a57c9aa319e1ee6fda6
ZamJunio <samjunio0123@gmail.com>
2026-01-31 18:36:15 -1100

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -48,3 +48,17 @@ public class calculator {
 
 }
 
+    System.out.print("Enter first number: ");
+    double num1 = input.nextDouble();
+
+    System.out.print("Enter second number: ");
+    double num2 = input.nextDouble();
+
+    double result = num1 * num2;
+    
+    System.out.println("Result: " + result);
+
+    input.close();
+
+    }
+}
