41b21649d5762e11312777cd4b0bbb98da01279c
ZamJunio <samjunio0123@gmail.com>
2026-01-31 18:53:19 -1100

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -45,13 +45,13 @@ public class calculator {
 
     public static int addition(int num1, int num2) {
         return num1 + num2;
-    ]
+    }
     public static int subtraction(int num1, int num2) {
         return num1 - num2;
     }
 
-}
   public static int multiplication(int num1, int num2) {
         return num1 * num2;
     }
 }
+
